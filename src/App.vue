76ec2ba7ecdca24718804f15a67dfd0d9a56f0d5
Alonso García Ascencio <alonso.acjc15@gmail.com>
2022-04-03 18:27:05 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> 
+    <router-link to="/sabores">Sabores</router-link>
+    <router-link to="/adornos">Adornos</router-link>
+    <router-link to="/seleccion">Seleccion</router-link>
+    <router-link to="/ordenes">Ordenes</router-link>
+    <router-link to="/pastelero">Pastelero</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: radial-gradient(circle, rgba(255,222,195,1) 0%, rgba(161,137,153,1) 100%);
+  height: 100%;
+  
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+nav {
+    width: 100%;
+    height: 50px;
+    background: #ffffff;
+    margin-top: 2%;
+}
+
+nav a {
+    width: 200px;
+    height: 10px;
+    color: #ffffff;
+    text-decoration: none;
+    margin: 0 20px;
+    padding-top: 8px;
+    background: #545454;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    border-radius: 1rem;
+}
+
+nav a.router-link-exact-active {
+  color: rgba(255,222,195,1);
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/img/logo.png">
+    <div class="table" id="PanBase">
+      <div>
+      <h1>Bienvenido a Pastel Soul</h1>
+      <h2>El pastel que alimenta tu alma</h2>
+
+      <p>
+      Nos encontramos en av. del centro no. 59 <br>
+      con un horario de atencion desde las 10:00 a.m. - 9:00 p.m. 
+      </p>
+    </div>
+    
+        <div class="article">
+
+            <h3>Tipos de harina</h3>
+            <table>
+                <tr>
+                    <th>Pan</th>
+                    <th>Precio</th>
+                </tr>
+                <tr>
+                    <td>Harina de trigo</td>
+                    <td>$50</td>
+                </tr>
+                <tr>
+                    <td>Harina de arroz</td>
+                    <td>$80</td>
+                </tr>
+                <tr>
+                    <td>Harina de maiz</td>
+                    <td>$80</td>
+                </tr>
+                <tr>
+                    <td>Harina de avena</td>
+                    <td>$100</td>
+                </tr>
+                <tr>
+                    <td>Harina de frutos secos</td>
+                    <td>$120</td>
+                </tr>
+              </table>
+              <div class="figure">
+                <img id="pastel" src="../assets/img/harina.jpg" alt="">
+              </div>
+        </div>
+    
+  
+    </div>    
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+ 
+}
+</script>
+<style scoped>
+.home{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h3{
+    color: #ffffff;
+    font-size: 3rem;
+    text-align: center;
+}
+table{
+    justify-content: center;
+    text-align:left;
+    padding-top: 5%;
+    padding-bottom: 3%;
+    border-collapse:separate ;
+    border-spacing: 15px 15px;
+
+        
+}
+h1,h2,p{
+    
+    height: 60px;
+    color: #383838;
+    padding-bottom: 40px;
+    font-size: 40px;
+    text-align: center;
+
+}
+
+.article{
+  
+  color: #ffffff;
+  font-size: larger ;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: #38383880;
+ 
+  border-radius: 20px; 
+}
+.figure img{
+  border-radius: 15px;
+}
+
+</style>
